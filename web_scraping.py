0bd476d170711758a55f77dd59fc60846c95fa9d
Khedri-999 <khedriamer1670@gmail.com>
2024-09-18 16:15:06 +0200

    first commit

---
+++ web_scraping.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import csv
+
+page = requests.get("https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue")
+
+src = page.content
+
+soup = BeautifulSoup(src, 'lxml')
+
+table = soup.find_all('table')[0]
+world_titles = table.find_all('th')
+world_table_titles = [title.text.strip() for title in world_titles]
+
+df = pd.DataFrame(columns=world_table_titles) 
+column_data = table.find_all('tr') 
+
+for row in column_data[1:] : 
+    row_data = row.find_all('td') 
+    individual_row_data = [data.text.strip() for data in row_data] 
+    length = len(df) 
+    df.loc[length] = individual_row_data 
+
+df.to_csv("largest_companies_usa.csv", index = False ) 
+print("Successe")
+
+
+
